bc3e63b32132105c7e2f1ec4be5654a9f84980c9
Yifeng Jiang <uprushworld@gmail.com>
2012-12-24 21:55:14 +0900

    Add myvim cookbook to configure my vim

---
+++ cookbooks/myvim/recipes/default.rb
@@ -0,0 +1,17 @@
+myuser = node[:myvim][:user]
+
+# the .vim directory
+remote_directory "/home/#{myuser}/.vim" do
+  source "vim"
+  owner myuser
+  group myuser
+  mode 0755
+end
+
+# .vimrc file
+cookbook_file "/home/#{myuser}/.vimrc" do
+  source "vimrc"
+  owner myuser
+  group myuser
+  mode 0644
+end
