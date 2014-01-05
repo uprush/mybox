1e8e5f6339ae54484a8a7746dcbea734a6e3c842
Yifeng Jiang <uprushworld@gmail.com>
2014-01-05 16:06:50 +0900

    Install chef-solo on bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,18 @@ HERE=`cd $HERE; pwd`
 
 git submodule update --init
 
+GEM=`which gem`
+CHEFSOLO=`which chef-solo`
+if [ ! -f "$CHEFSOLO" ]; then
+  if [ "$GEM" == "/usr/bin/gem" ]; then
+    sudo gem install chef
+  else
+    gem install chef
+  fi
+else
+  echo "Found $CHEFSOLO"
+fi
+
 sudo mkdir -p /var/chef/cache
 if [ ! -L /var/chef/cookbooks ]; then
   sudo ln -s "$HERE/cookbooks" /var/chef/cookbooks
